30f2484f1fe74c8b9dcb19cb105e5cf5ca3a9670
honuKdestine <honudestine933@gmail.com>
2026-02-04 01:57:41 +0000

    Custom user model setup

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# import the User model from the models module
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "role", "is_active", "is_staff", "created_at")
+    list_filter = ("role", "is_active")
+    search_fields = ("email", "role")
--- users/models.py
+++ users/models.py
@@ -1,3 +1,51 @@
+# imports the models module from django.db package
 from django.db import models
+# imports used when creating custom user model
+# allows for overriding the default User model to fit specific needs(eg; email auth instead of username for login)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", User.Role.SUPERADMIN)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Role(models.TextChoices):
+        SUPERADMIN = "SUPERADMIN", "Super Admin"
+        ADMIN = "ADMIN", "Transport Admin"
+        DRIVER = "DRIVER", "Driver"
+        PASSENGER = "PASSENGER", "Passenger"
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=50, choices=Role.choices, default=Role.PASSENGER)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return F"{self.email} ({self.role})"
